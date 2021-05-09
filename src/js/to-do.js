0f674240b10f8300feb5debb795516f23837ef5b
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2021-05-10 01:07:21 +0430

    show/hide add to do form

--- src/js/to-do.js
+++ src/js/to-do.js
@@ -1,1 +1,14 @@
-window.onload = (function () {})();
+window.onload = (function () {
+  // Display form
+  var todoAddButton = document.querySelector("#to-do-add-button");
+  var todoCloseButton = document.querySelector("#to-do-close-button");
+  var todoFormWrapper = document.querySelector("#to-do-form-wrapper");
+
+  todoAddButton.addEventListener("click", function () {
+    todoFormWrapper.style.display = "flex";
+  });
+
+  todoCloseButton.addEventListener("click", function () {
+    todoFormWrapper.style.display = "none";
+  });
+})();
