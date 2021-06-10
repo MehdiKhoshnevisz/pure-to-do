b5bce089baddb2cd7bdf48346f893961544872e0
Mehdi Khoshnevisz <mkhnsz1996@gmail.com>
2021-06-11 04:23:56 +0430

    feat: Add to-do to list

--- src/js/to-do.js
+++ src/js/to-do.js
@@ -1,14 +1,36 @@
 window.onload = (function () {
-  // Display form
+  var toDoForm = document.querySelector('.to-do-form');
   var todoAddButton = document.querySelector("#to-do-add-button");
+  var firstToDoNode = document.querySelectorAll('.to-do-element')[0];
   var todoCloseButton = document.querySelector("#to-do-close-button");
   var todoFormWrapper = document.querySelector("#to-do-form-wrapper");
+  var todoAddToListButton = document.querySelector('#to-do-add-to-list-button');
 
-  todoAddButton.addEventListener("click", function () {
+  todoAddButton.addEventListener("click", showToDoForm);
+  todoCloseButton.addEventListener("click", hideToDoForm);
+  todoAddToListButton.addEventListener("click", addToDoToList);
+
+  function showToDoForm() {
     todoFormWrapper.style.display = "flex";
-  });
+  }
 
-  todoCloseButton.addEventListener("click", function () {
+  function hideToDoForm() {
     todoFormWrapper.style.display = "none";
-  });
+  }
+
+  function addToDoToList() {
+    var toDoFormTitle = document.querySelector('#to-do-form-title').value;
+    var toDoFormDescription = document.querySelector('#to-do-form-description').value;
+    
+    var newToDoNode = firstToDoNode.cloneNode(true);
+
+    newToDoNode.querySelector('.to-do-element__title').innerText = toDoFormTitle; 
+    newToDoNode.querySelector('.to-do-element__description').innerText = toDoFormDescription;
+
+    document.querySelector('.to-do-wrapper').appendChild(newToDoNode);
+
+    toDoForm.reset();
+
+    hideToDoForm();
+  }
 })();
